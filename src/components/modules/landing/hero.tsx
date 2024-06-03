1232110ee9ff96702eec7204e179325c9e646194
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2024-06-03 23:38:53 +0530

    finished hero section layout

--- src/components/modules/landing/hero.tsx
+++ src/components/modules/landing/hero.tsx
@@ -5,29 +5,27 @@ import React from "react";
 export default function Hero() {
   return (
     <>
-      <div className="h-[calc(100vh-100px)] flex justify-center items-center">
-        <Wrapper>
-          <div className="flex items-center justify-between">
-            <div className="w-full flex flex-col">
-              <h1 className="text-6xl font-bold">
-                Build like a team of hundreds_
-              </h1>
-              <div className="">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Sapiente, ad?
-              </div>
-            </div>
-            <div className="p-5 w-full space-y-12">
-              <div className="font-medium text-2xl">
-                Build your entire backend within minutes and scale effortlessly
-                using Appwrite&apos;s open-source platform. Add Authentication,
-                Databases, Functions, Storage, and Messaging to your projects
-                using the frameworks and languages of your choice.
-              </div>
-              <Button>Shop Now</Button>
+      <div className="h-[calc(100vh-100px)] flex justify-center items-center gap-4 px-32">
+        <div className="flex py-6">
+          <div className="w-full flex justify-between flex-col">
+            <h1 className="text-6xl font-bold">
+              Build like a team of hundreds_
+            </h1>
+            <div className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
+              ad?
             </div>
           </div>
-        </Wrapper>
+          <div className="w-full space-y-5 px-6">
+            <div className="font-medium text-2xl">
+              Build your entire backend within minutes and scale effortlessly
+              using Appwrite&apos;s open-source platform. Add Authentication,
+              Databases, Functions, Storage, and Messaging to your projects
+              using the frameworks and languages of your choice.
+            </div>
+            <Button>Shop Now</Button>
+          </div>
+        </div>
       </div>
     </>
   );
