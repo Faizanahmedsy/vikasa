8368cb0b6246a41ef873125f053169c56d20c879
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2024-06-13 08:03:37 +0530

    fixed not scrooling issue

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,7 +1,6 @@
 import HeroFormCenterAlignedWithAForm from "@/components/kitty/kitty-cta";
-import { Banner, Categories, Hero, WhyUs } from "@/components/modules/landing";
+import { Categories, Hero, WhyUs } from "@/components/modules/landing";
 import Intro from "@/components/modules/landing/intro";
-import { IconCloudDemo } from "@/components/modules/landing/tech-cloud";
 import Testimonials from "@/components/modules/landing/testimonials";
 
 export default function HomePage() {
